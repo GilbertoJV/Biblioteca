79409bb7b70266b4f4ffe30de909cab19eaf2580
GilbertoJV <skirlo123aa@gmail.com>
2021-08-26 16:53:39 -0500

    first commit

---
+++ biblioteca/settings/local.py
@@ -0,0 +1,29 @@
+from .base import *
+import os
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbbiblioteca',
+        'USER': 'postgres',
+        'PASSWORD': '@min123.',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
